e29b719b018d06c1c40e0a3b3748b48b67c70959
Håkon Rossebø <hakonrossebo@hotmail.com>
2017-05-01 22:22:34 +0200

    Logging og options

--- backend/src/VAF.Aktivitetsbank.API/Controllers/AktivitetController.cs
+++ backend/src/VAF.Aktivitetsbank.API/Controllers/AktivitetController.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using VAF.Aktivitetsbank.Application;
 using VAF.Aktivitetsbank.Application.Commands;
 using VAF.Aktivitetsbank.Application.Handlers;
@@ -15,17 +17,22 @@ namespace VAF.Aktivitetsbank.API.Controllers
     {
         private readonly IQueryDispatcher _queryDispatcher;
         private readonly ICommandDispatcher _commandDispatcher;
+        private readonly ILogger<UserController> _logger;
+        private readonly AppOptions _options;
 
-        public AktivitetController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+        public AktivitetController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher, IOptions<AppOptions> options, ILogger<UserController> logger )
         {
             _queryDispatcher = queryDispatcher;
             _commandDispatcher = commandDispatcher;
+            _logger = logger;
+            _options = options.Value;
         }
 
         //[HttpGet("{query}")]
         [HttpGet]
         public IEnumerable<AktivitetDto> Get()
         {
+            _logger.LogInformation("Lister ut aktiviteter");
             return _queryDispatcher.Query<AktivitetSearchQuery, IList<AktivitetDto>>(new AktivitetSearchQuery("")).ToList();
         }
 
