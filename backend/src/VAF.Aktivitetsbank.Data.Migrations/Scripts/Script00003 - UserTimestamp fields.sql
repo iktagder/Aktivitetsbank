c4b04f2f01576296f4aefacbbfad3b2ff350c3b8
Håkon Rossebø <hakonrossebo@hotmail.com>
2017-05-14 23:49:01 +0200

    Nye felt for bruker og dato - migreringsscript

---
+++ backend/src/VAF.Aktivitetsbank.Data.Migrations/Scripts/Script00003 - UserTimestamp fields.sql
@@ -0,0 +1,72 @@
+﻿ALTER TABLE [Deltaker] ADD [Endret] datetime2;
+
+GO
+
+ALTER TABLE [Deltaker] ADD [EndretAv] nvarchar(max);
+
+GO
+
+ALTER TABLE [Deltaker] ADD [Opprettet] datetime2;
+
+GO
+
+ALTER TABLE [Deltaker] ADD [OpprettetAv] nvarchar(max);
+
+GO
+
+ALTER TABLE [Aktivitet] ADD [Endret] datetime2;
+
+GO
+
+ALTER TABLE [Aktivitet] ADD [EndretAv] nvarchar(max);
+
+GO
+
+ALTER TABLE [Aktivitet] ADD [Opprettet] datetime2;
+
+GO
+
+ALTER TABLE [Aktivitet] ADD [OpprettetAv] nvarchar(max);
+
+GO
+
+UPDATE dbo.Aktivitet SET Opprettet = GETDATE()
+              where Opprettet IS NULL;
+
+GO
+
+UPDATE dbo.Aktivitet SET Endret = GETDATE()
+              where Endret IS NULL;
+
+GO
+
+UPDATE dbo.Deltaker SET Opprettet = GETDATE()
+              where Opprettet IS NULL;
+
+GO
+
+UPDATE dbo.Deltaker SET Endret = GETDATE()
+              where Endret IS NULL;
+
+GO
+
+
+UPDATE dbo.Aktivitet SET OpprettetAv = 'System'
+              where OpprettetAv IS NULL;
+
+GO
+
+UPDATE dbo.Aktivitet SET EndretAv = 'System'
+              where EndretAv IS NULL;
+
+GO
+
+UPDATE dbo.Deltaker SET OpprettetAv = 'System'
+              where OpprettetAv IS NULL;
+
+GO
+
+UPDATE dbo.Deltaker SET EndretAv = 'System'
+              where EndretAv IS NULL;
+
+GO
---
+++ backend/src/VAF.Aktivitetsbank.Data.Migrations/Scripts/Script00004 - UserTimestamp fields not null.sql
@@ -0,0 +1,80 @@
+﻿DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Deltaker') AND [c].[name] = N'Opprettet');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Deltaker] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [Deltaker] ALTER COLUMN [Opprettet] datetime2 NOT NULL;
+
+GO
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Deltaker') AND [c].[name] = N'Endret');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Deltaker] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [Deltaker] ALTER COLUMN [Endret] datetime2 NOT NULL;
+
+GO
+
+DECLARE @var2 sysname;
+SELECT @var2 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Aktivitet') AND [c].[name] = N'Opprettet');
+IF @var2 IS NOT NULL EXEC(N'ALTER TABLE [Aktivitet] DROP CONSTRAINT [' + @var2 + '];');
+ALTER TABLE [Aktivitet] ALTER COLUMN [Opprettet] datetime2 NOT NULL;
+
+GO
+
+DECLARE @var3 sysname;
+SELECT @var3 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Aktivitet') AND [c].[name] = N'Endret');
+IF @var3 IS NOT NULL EXEC(N'ALTER TABLE [Aktivitet] DROP CONSTRAINT [' + @var3 + '];');
+ALTER TABLE [Aktivitet] ALTER COLUMN [Endret] datetime2 NOT NULL;
+
+GO
+
+
+DECLARE @var4 sysname;
+SELECT @var4 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Deltaker') AND [c].[name] = N'OpprettetAv');
+IF @var4 IS NOT NULL EXEC(N'ALTER TABLE [Deltaker] DROP CONSTRAINT [' + @var4 + '];');
+ALTER TABLE [Deltaker] ALTER COLUMN [OpprettetAv] nvarchar(max) NOT NULL;
+
+GO
+
+DECLARE @var5 sysname;
+SELECT @var5 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Deltaker') AND [c].[name] = N'EndretAv');
+IF @var5 IS NOT NULL EXEC(N'ALTER TABLE [Deltaker] DROP CONSTRAINT [' + @var5 + '];');
+ALTER TABLE [Deltaker] ALTER COLUMN [EndretAv] nvarchar(max) NOT NULL;
+
+GO
+
+DECLARE @var6 sysname;
+SELECT @var6 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Aktivitet') AND [c].[name] = N'OpprettetAv');
+IF @var6 IS NOT NULL EXEC(N'ALTER TABLE [Aktivitet] DROP CONSTRAINT [' + @var6 + '];');
+ALTER TABLE [Aktivitet] ALTER COLUMN [OpprettetAv] nvarchar(max) NOT NULL;
+
+GO
+
+DECLARE @var7 sysname;
+SELECT @var7 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Aktivitet') AND [c].[name] = N'EndretAv');
+IF @var7 IS NOT NULL EXEC(N'ALTER TABLE [Aktivitet] DROP CONSTRAINT [' + @var7 + '];');
+ALTER TABLE [Aktivitet] ALTER COLUMN [EndretAv] nvarchar(max) NOT NULL;
+
+GO
