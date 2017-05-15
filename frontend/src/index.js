48bcb3901dc949a5ab559c2c1af68e7e157e29f9
Håkon Rossebø <hakonrossebo@hotmail.com>
2017-05-15 09:02:58 +0200

    Octopus deploy variable substitution

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,10 @@ require('ace-css/css/ace.css');
 
 var Elm = require('./Main.elm');
 
+var configuredApiUrl = "#{ApiUrl}";
+apiUrl = (configuredApiUrl[0] !== "#") ? configuredApiUrl : API_URL;
+
 var myJSTestApp = Elm.Main.embed(document.getElementById('root'), {
 	currentTime: Date.now(),
-	apiEndpoint: API_URL
+	apiEndpoint: apiUrl
 });
