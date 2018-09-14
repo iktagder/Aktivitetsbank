3e2a8e6d464f6a0de964cc0377eb8de85721bb86
Ole Anders Eidjord <oae@vaf.no>
2018-09-14 16:29:48 +0200

    Fiks for bilde til nordland

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 require('./main.css')
-require('file?name=/images/elm.png!../images/elm.png')
+
+require('file?name=/images/nfk-logo.png!../images/nfk-logo.png')
+require('file?name=/images/vaf-logo.png!../images/vaf-logo.png')
+require('file?name=/images/vaf-logo.svg!../images/vaf-logo.svg')
+
 require('ace-css/css/ace.css')
 
 var Elm = require('./Main.elm')
 
 var configuredLogo = '#{Logo}'
-var logo = (configuredLogo[0] !== '#') ? configuredLogo : './src/vaf-logo.png'
+var logo = (configuredLogo[0] !== '#') ? configuredLogo : './images/vaf-logo.png'
 
 var configuredApiUrl = '#{ApiUrl}'
 var apiUrl = (configuredApiUrl[0] !== '#') ? configuredApiUrl : API_URL
